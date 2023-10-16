12546ecdba56a969074f49990d6c47f02fb2c2fe
ryanmio <ryan@mioduski.us>
2023-10-16 19:27:21 -0400

    Update index.js

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Auth from '../components/Auth';
 import GameModeSelection from '../components/GameModeSelection';
 import CodeSnippetDisplay from '../components/CodeSnippetDisplay';
@@ -6,16 +6,31 @@ import UserAnswerInput from '../components/UserAnswerInput';
 
 console.log(process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
-
 export default function Home() {
+  const [user, setUser] = useState(null);
+  const [gameMode, setGameMode] = useState(null);
+
+  const handleUserAuth = (user) => {
+    setUser(user);
+  };
+
+  const handleGameModeSelect = (mode) => {
+    setGameMode(mode);
+  };
+
   return (
     <div>
       <h1>DecodeMe!</h1>
-      <Auth />
-      <GameModeSelection />
-      <CodeSnippetDisplay />
-      <UserAnswerInput />
+      {!user ? (
+        <Auth onUserAuth={handleUserAuth} />
+      ) : !gameMode ? (
+        <GameModeSelection onGameModeSelect={handleGameModeSelect} />
+      ) : (
+        <>
+          <CodeSnippetDisplay gameMode={gameMode} />
+          <UserAnswerInput gameMode={gameMode} />
+        </>
+      )}
     </div>
   );
 }
-
