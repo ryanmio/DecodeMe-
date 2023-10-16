24469268b87cdee97a78a35bd64e14b61f8f88e2
ryanmio <ryan@mioduski.us>
2023-10-16 08:19:13 -0400

    Create Auth.js

---
+++ frontend/components/Auth.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import firebase from '../firebase';
+
+export default function Auth() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signUp = () => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // User registered
+        var user = userCredential.user;
+      })
+      .catch((error) => {
+        // Handle errors
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+  };
+
+  const signIn = () => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // User signed in
+        var user = userCredential.user;
+      })
+      .catch((error) => {
+        // Handle errors
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+  };
+
+  return (
+    <div>
+      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={signUp}>Sign Up</button>
+      <button onClick={signIn}>Sign In</button>
+    </div>
+  );
+}
