7ac0dfec977f4fe9e488f6527b228a1f80338d19
ryanmio <ryan@mioduski.us>
2023-10-16 08:27:22 -0400

    Revert "Update firebase.js"
    This reverts commit 91426080c2b0165a09d29d37f15eb8cc909ec4e2.

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAtAACIrw85HlBz25WuTqGmVVUbb6MJvQ8",
+  authDomain: "decodeme-1f38e.firebaseapp.com",
+  projectId: "decodeme-1f38e",
+  storageBucket: "decodeme-1f38e.appspot.com",
+  messagingSenderId: "141094552346",
+  appId: "1:141094552346:web:49fd9edc9e829f558da2a3",
+  measurementId: "G-GFPBL46PKD"
 };
 
 if (!firebase.apps.length) {
