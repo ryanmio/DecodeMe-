b62e6ad08d45a7e550c04fa0ddf21aafcc12dea1
ryanmio <ryan@mioduski.us>
2023-10-16 19:40:46 -0400

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,42 +1,42 @@
 const functions = require('firebase-functions');
 const axios = require('axios');
 
-exports.getGpt4Chat = functions.https.onRequest(async (request, response) => {
-    const userMessage = request.body.userMessage;
-    if (!userMessage) {
-        console.error('No user message provided.');
-        return response.status(400).send('Please provide a user message.');
-    }
-    console.log(`Received user message: ${userMessage}`);
+exports.getCodeSnippet = functions.https.onRequest(async (request, response) => {
+  const gameMode = request.query.gameMode;
+  if (!gameMode) {
+    console.error('No game mode provided.');
+    return response.status(400).send('Please provide a game mode.');
+  }
+  console.log(`Received game mode: ${gameMode}`);
 
-    const openaiKey = functions.config().openai?.key;
-    if (!openaiKey) {
-        console.error('OpenAI key missing from Firebase function configuration.');
-        return response.status(500).send('Server configuration error.');
-    }
+  const openaiKey = functions.config().openai?.key;
+  if (!openaiKey) {
+    console.error('OpenAI key missing from Firebase function configuration.');
+    return response.status(500).send('Server configuration error.');
+  }
 
-    const apiUrl = 'https://api.openai.com/v1/chat/completions';
-    const headers = {
-        'Authorization': `Bearer ${functions.config().openai.key}`,
-        'Content-Type': 'application/json',
-    };
-    const data = {
-        model: 'gpt-3.5-turbo',
-        messages: [
-            { role: 'system', content: 'You are a helpful assistant.' },
-            { role: 'user', content: userMessage }
-        ]
-    };
+  const apiUrl = 'https://api.openai.com/v1/chat/completions';
+  const headers = {
+    'Authorization': `Bearer ${openaiKey}`,
+    'Content-Type': 'application/json',
+  };
+  const data = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: `Lets play a game that also teaches me python scripts. You will write a short python script in a code block and give me two multiple choice options for what the code does. If I get it right, continue with a slightly harder script. If I get it wrong, continue with a similar script. Dont stop until I say so.` }
+    ]
+  };
 
-    try {
-        const openaiResponse = await axios.post(apiUrl, data, { headers: headers });
-        console.log(`Received response from OpenAI: ${JSON.stringify(openaiResponse.data)}`);
-        response.send(openaiResponse.data.choices[0].message.content.trim());
-    } catch (error) {
-        console.error('Error occurred while communicating with OpenAI:', error);
-        if (error.response) {
-            console.error('Error Response:', error.response.data);
-        }
-        response.status(500).send('An error occurred while communicating with OpenAI.');
+  try {
+    const openaiResponse = await axios.post(apiUrl, data, { headers: headers });
+    console.log(`Received response from OpenAI: ${JSON.stringify(openaiResponse.data)}`);
+    response.send({ codeSnippet: openaiResponse.data.choices[0].message.content.trim() });
+  } catch (error) {
+    console.error('Error occurred while communicating with OpenAI:', error);
+    if (error.response) {
+      console.error('Error Response:', error.response.data);
     }
+    response.status(500).send('An error occurred while communicating with OpenAI.');
+  }
 });
