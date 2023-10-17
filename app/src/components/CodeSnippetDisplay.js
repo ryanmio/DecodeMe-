769d8e11db790624a45329bcde9c55f9448ece67
ryanmio <ryan@mioduski.us>
2023-10-17 08:33:09 -0400

    Update CodeSnippetDisplay.js

--- app/src/components/CodeSnippetDisplay.js
+++ app/src/components/CodeSnippetDisplay.js
@@ -4,20 +4,32 @@ import PropTypes from 'prop-types';
 async function fetchCodeSnippet(gameMode) {
   const response = await fetch(`/api/code-snippets?gameMode=${gameMode}`);
   const data = await response.json();
-  return data.codeSnippet;
+  return data;
 }
 
 export default function CodeSnippetDisplay({ gameMode }) {
   const [codeSnippet, setCodeSnippet] = useState('');
+  const [options, setOptions] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState('');
 
   useEffect(() => {
-    fetchCodeSnippet(gameMode).then(setCodeSnippet);
+    fetchCodeSnippet(gameMode).then(data => {
+      setCodeSnippet(data.codeSnippet);
+      setOptions(data.options);
+      setCorrectAnswer(data.correctAnswer);
+    });
   }, [gameMode]);
 
   return (
     <div>
       <h1>Code Snippet</h1>
       <pre>{codeSnippet}</pre>
+      <h2>Options</h2>
+      <ul>
+        {options.map((option, index) => (
+          <li key={index}>{option}</li>
+        ))}
+      </ul>
     </div>
   );
 }
