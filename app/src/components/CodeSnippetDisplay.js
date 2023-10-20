04ba3a88ed8dd3755c8574ea48ffa2f5c69cf67e
ryanmio <ryan@mioduski.us>
2023-10-20 08:03:30 -0400

    Update CodeSnippetDisplay.js

--- app/src/components/CodeSnippetDisplay.js
+++ app/src/components/CodeSnippetDisplay.js
@@ -2,21 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import dynamic from 'next/dynamic';
 
-
 const SyntaxHighlighter = dynamic(
-  import('react-syntax-highlighter/dist/cjs/prism').then(mod => mod.Prism),
+  () => import('react-syntax-highlighter/dist/cjs/prism').then(mod => mod.Prism),
   { ssr: false }
 );
 const solarizedlight = dynamic(
-  import('react-syntax-highlighter/dist/cjs/styles/prism').then(mod => mod.solarizedlight),
+  () => import('react-syntax-highlighter/dist/cjs/styles/prism/solarizedlight'),
   { ssr: false }
 );
 
 export default function CodeSnippetDisplay({ codeSnippet }) {
+  // Remove backticks from the code snippet
+  const formattedCodeSnippet = codeSnippet ? codeSnippet.replace(/```/g, '') : '';
+
   return (
     <div>
       <h1>Code Snippet</h1>
-      <pre>{codeSnippet}</pre>
+      {formattedCodeSnippet ? (
+        <SyntaxHighlighter language="python" style={solarizedlight}>
+          {formattedCodeSnippet}
+        </SyntaxHighlighter>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
