bf4fc47bf434b5d909622227232647cff0a1c200
ryanmio <ryan@mioduski.us>
2023-10-26 19:56:43 -0400

    Update CodeSnippetDisplay.js
    "Write this compactly, keep all functionality"

--- app/src/components/CodeSnippetDisplay.js
+++ app/src/components/CodeSnippetDisplay.js
@@ -4,17 +4,8 @@ import { CodeBlock, dracula } from 'react-code-blocks';
 import { useSpring, animated } from '@react-spring/web';
 
 export default function CodeSnippetDisplay({ codeSnippet, loading }) {
-  const formattedCodeSnippet = codeSnippet 
-    ? codeSnippet.replace(/```python\n|```python|```/g, '').trim() 
-    : '';
-
-   // Spring for loading state
-   const fadeLoading = useSpring({
-    from: { opacity: 0 },
-    to: { opacity: 1 },
-    delay: 200,
-    config: { tension: 200, friction: 20 },
-  });
+  const formattedCodeSnippet = codeSnippet?.replace(/```python\n|```python|```/g, '').trim() || '';
+  const fadeLoading = useSpring({ from: { opacity: 0 }, to: { opacity: 1 }, delay: 200, config: { tension: 200, friction: 20 } });
 
   return (
     <div>
@@ -22,22 +13,10 @@ export default function CodeSnippetDisplay({ codeSnippet, loading }) {
       <div className="max-w-[600px] mx-auto">
         {loading ? (
           <animated.div style={fadeLoading}>
-            <CodeBlock
-              text={"Loading..." + "\n".repeat(5)}
-              language={"plaintext"}
-              showLineNumbers={false}
-              theme={dracula}
-              wrapLines
-            />
+            <CodeBlock text={"Loading..." + "\n".repeat(5)} language={"plaintext"} showLineNumbers={false} theme={dracula} wrapLines />
           </animated.div>
         ) : (
-          <CodeBlock
-            text={formattedCodeSnippet}
-            language={"python"}
-            showLineNumbers={true}
-            theme={dracula}
-            wrapLines
-          />
+          <CodeBlock text={formattedCodeSnippet} language={"python"} showLineNumbers={true} theme={dracula} wrapLines />
         )}
       </div>
     </div>
