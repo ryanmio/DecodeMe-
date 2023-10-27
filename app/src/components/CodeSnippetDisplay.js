f8bd08c04496003be69bee7fa958faaeb4207820
ryanmio <ryan@mioduski.us>
2023-10-27 08:03:39 -0400

    updated loading state

--- app/src/components/CodeSnippetDisplay.js
+++ app/src/components/CodeSnippetDisplay.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CodeBlock, dracula } from 'react-code-blocks';
 import { useSpring, animated } from '@react-spring/web';
+import { Ping } from '@uiball/loaders';
 
 export default function CodeSnippetDisplay({ codeSnippet, loading }) {
   const formattedCodeSnippet = codeSnippet?.replace(/```python\n|```python|```/g, '').trim() || '';
@@ -10,13 +11,12 @@ export default function CodeSnippetDisplay({ codeSnippet, loading }) {
   return (
     <div>
       <h2 className="text-xl font-medium mb-3 text-left text-gray-900">Code Snippet</h2>
-      <div className="max-w-[600px] mx-auto">
-        {loading ? (
-          <animated.div style={fadeLoading}>
-            <CodeBlock text={"Loading..." + "\n".repeat(5)} language={"plaintext"} showLineNumbers={false} theme={dracula} wrapLines />
+      <div className="max-w-[600px] mx-auto relative">
+        <CodeBlock text={formattedCodeSnippet || " " + "\n".repeat(5)} language={"python"} showLineNumbers={true} theme={dracula} wrapLines />
+        {loading && (
+          <animated.div style={fadeLoading} className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <Ping size={45} speed={2} color="white" />
           </animated.div>
-        ) : (
-          <CodeBlock text={formattedCodeSnippet} language={"python"} showLineNumbers={true} theme={dracula} wrapLines />
         )}
       </div>
     </div>
--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -1,4 +1,5 @@
 import '../tailwind.css';
+import '../Style.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
