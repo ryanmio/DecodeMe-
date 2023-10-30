1e3b4ace020d6de825853b565a5162e3af05abc9
ryanmio <ryan@mioduski.us>
2023-10-30 08:24:33 -0400

    logout in menu

--- app/src/components/OptionsMenu.js
+++ app/src/components/OptionsMenu.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
 import { IoOptions } from "react-icons/io5";
+import { getFirebaseAuth } from '../firebase'; // Corrected import
+import { signOut } from 'firebase/auth'; // Import signOut
 
 const OptionsMenu = () => {
+  const handleLogout = async () => {
+    const auth = getFirebaseAuth();
+    try {
+      await signOut(auth);
+      window.location.href = '/';
+    } catch (error) {
+      console.error('Failed to log out:', error);
+    }
+  };
+
   return (
     <Dropdown>
       <DropdownTrigger>
-      <Button
-  variant="flat"
-  auto
-  className="custom-button"
-  style={{ background: 'transparent' }}
->
-  <IoOptions size={24} color="#22D3EE" />
-</Button>
+        <Button
+          variant="flat"
+          auto
+          className="custom-button"
+          style={{ background: 'transparent' }}
+        >
+          <IoOptions size={24} color="#22D3EE" />
+        </Button>
       </DropdownTrigger>
       <DropdownMenu variant="shadow" aria-label="Options menu">
         <DropdownItem key="hint" shortcut="⌘H">Hint</DropdownItem>
-        <DropdownItem key="logout" shortcut="⌘L">Logout</DropdownItem>
+        <DropdownItem key="logout" shortcut="⌘L" onClick={handleLogout}>Logout</DropdownItem>
         <DropdownItem key="skip" shortcut="⌘S">Skip/Refresh</DropdownItem>
         <DropdownItem key="history" shortcut="⌘H">Past Questions</DropdownItem>
         <DropdownItem key="textSize" shortcut="⌘T">Text Size</DropdownItem>
