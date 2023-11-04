dcb7dd75a653600f8f0bd0285a81c61e2f55da55
ryanmio <ryan@mioduski.us>
2023-11-04 16:40:22 -0400

    python button size hover state

--- app/src/components/PythonChip.js
+++ app/src/components/PythonChip.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Chip, Avatar } from '@nextui-org/react';
 
 const PythonChip = () => {
+  const [hover, setHover] = React.useState(false);
+
   return (
     <Chip
       variant="faded"
@@ -12,6 +14,12 @@ const PythonChip = () => {
           alt="Python"
         />
       }
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      style={{
+        transform: hover ? 'scale(1)' : 'scale(0.85)',
+        transition: 'transform 0.3s ease-in-out',
+      }}
     >
       Python
     </Chip>
