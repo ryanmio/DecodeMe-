fcbdd471f0d0c108b0bd5792fc8dfa5250099f32
ryanmio <ryan@mioduski.us>
2023-11-05 15:51:31 -0500

    hot keys

--- app/src/components/OptionsMenu.js
+++ app/src/components/OptionsMenu.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
 import { IoOptions } from "react-icons/io5";
 import { getFirebaseAuth } from '../firebase';
 import { signOut } from 'firebase/auth';
+import { useHotkeys } from 'react-hotkeys-hook';
 
 const OptionsMenu = () => {
   const handleLogout = async () => {
+    console.log('handleLogout called');
     const confirmLogout = window.confirm("Are you sure you want to log out?");
     if (confirmLogout) {
       const auth = getFirebaseAuth();
@@ -17,6 +19,13 @@ const OptionsMenu = () => {
       }
     }
   };
+
+  useHotkeys('shift+l', handleLogout);
+
+  // add other hotkeys here
+  // useHotkeys('command+s', handleSkip);
+  // useHotkeys('command+h', handleHistory);
+  // useHotkeys('command+t', handleTextSize);
 
   return (
     <Dropdown>
@@ -32,7 +41,7 @@ const OptionsMenu = () => {
       </DropdownTrigger>
       <DropdownMenu variant="shadow" aria-label="Options menu">
         {/* <DropdownItem key="hint" shortcut="⌘H">Hint</DropdownItem> */}
-        <DropdownItem key="logout" shortcut="⌘L" onClick={handleLogout}>Logout</DropdownItem>
+        <DropdownItem key="logout" shortcut="⇧L" onClick={handleLogout}>Logout</DropdownItem>
         {/* <DropdownItem key="skip" shortcut="⌘S">Skip/Refresh</DropdownItem> */}
         {/* <DropdownItem key="history" shortcut="⌘H">Past Questions</DropdownItem> */}
         {/* <DropdownItem key="textSize" shortcut="⌘T">Text Size</DropdownItem> */}
