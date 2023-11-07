6fc202501990cb7355a0aff4588f3847892b578c
ryanmio <ryan@mioduski.us>
2023-11-07 18:30:27 -0500

    share results saves new collection

--- app/src/components/GameOver.js
+++ app/src/components/GameOver.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, addDoc, doc, writeBatch } from 'firebase/firestore';
 import { motion } from 'framer-motion';
 import { CodeBlock, dracula } from 'react-code-blocks';
 
@@ -36,6 +36,63 @@ const GameOver = ({ score, questionLimit, db, gameId, userId }) => {
     fetchGameHistory();
   }, [db, gameId, userId]);
 
+  const handleShareResults = async () => {
+    if (!gameId || !userId) {
+      console.error('Cannot share results because gameId or userId is not set:', { gameId, userId });
+      return;
+    }
+
+    setLoading(true);
+    setError('');
+
+    try {
+      // Generate a unique identifier for the share
+      const shareId = `${userId}_${Date.now()}`;
+
+      // Start a batch
+      const batch = writeBatch(db);
+
+      // Reference to the user's game history
+      const historyCollectionRef = collection(db, 'users', userId, 'games', gameId, 'history');
+
+      // Fetch the user's game history
+      const querySnapshot = await getDocs(historyCollectionRef);
+
+      // Reference to the public share collection
+      const shareCollectionRef = collection(db, 'sharedResults');
+
+      // Create a new document for sharing with the generated unique identifier
+      const shareDocRef = doc(shareCollectionRef, shareId);
+      batch.set(shareDocRef, {
+        gameId,
+        userId,
+        score,
+        questionLimit,
+        sharedAt: new Date(),
+      });
+
+      // Add each history item to the shared document
+      querySnapshot.forEach((historyDoc) => {
+        const historyData = historyDoc.data();
+        // We use `doc` function to create a reference to a new document inside the shared document
+        const historyDocRef = doc(shareDocRef, 'history', historyDoc.id);
+        batch.set(historyDocRef, historyData);
+      });
+      
+
+      // Commit the batch
+      await batch.commit();
+
+      // Provide feedback to the user that their results are shared
+      alert(`Your results are shared with ID: ${shareId}`);
+    } catch (error) {
+      console.error('Error sharing game history:', error);
+      setError('Failed to share game history.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="text-center">
       <motion.div
@@ -45,6 +102,13 @@ const GameOver = ({ score, questionLimit, db, gameId, userId }) => {
       >
         <h2 className="text-2xl font-bold mb-4">Round over!</h2>
         <p className="text-lg">Your score is {score} out of {questionLimit}.</p>
+        <button 
+        onClick={handleShareResults} 
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+        disabled={loading}
+      >
+        {loading ? 'Sharing...' : 'Share Results'}
+      </button>
         {loading && <p>Loading game history...</p>}
         {error && <p className="text-red-500">{error}</p>}
         <ul className="history-list">
