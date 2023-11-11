2a6947e16f8a33b528b5a9f3964a75ed38ea7bcf
ryanmio <ryan@mioduski.us>
2023-11-10 20:27:53 -0500

    Update ChallengeSection.js

--- app/src/components/ChallengeSection.js
+++ app/src/components/ChallengeSection.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 const ChallengeSection = () => {
   return (
     <div className="challenge-section text-center py-6">
-      <h2 className="text-2xl font-bold mb-4">Think You Can Beat My Score? Prove It!</h2>
+      <h2 className="text-xl font-bold mb-4">Think You Can Beat My Score? Prove It!</h2>
       <p className="text-lg mb-4">Take the DecodeMe Challenge!</p>
       <Link href="/" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
         Play DecodeMe! »
