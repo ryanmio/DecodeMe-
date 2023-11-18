97fab1676cdb8a23555cb4426323ae797e805f43
ryanmio <ryan@mioduski.us>
2023-11-18 16:46:21 -0500

    Update OptionsMenu.js

--- app/src/components/OptionsMenu.js
+++ app/src/components/OptionsMenu.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from 'next/router';
+import { getAuth, isSignInWithEmailLink } from 'firebase/auth';
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure } from "@nextui-org/react";
 import { IoOptions } from "react-icons/io5";
 import { getFirebaseAuth } from '../firebase';
 import { signOut } from 'firebase/auth';
 import { useHotkeys } from 'react-hotkeys-hook';
+import Auth from './Auth';
 
 const OptionsMenu = () => {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
+  const [showAuthModal, setShowAuthModal] = useState(false);
+  const router = useRouter();
+  const auth = getFirebaseAuth();
+
   const handleLogout = async () => {
     console.log('handleLogout called');
     onOpen();
@@ -20,6 +27,35 @@ const OptionsMenu = () => {
     } catch (error) {
       console.error('Failed to log out:', error);
     }
+  };
+
+  const handleHistory = () => {
+    console.log('handleHistory called');
+    const user = auth.currentUser;
+    console.log('Current user:', user);
+    if (user) {
+      if (!user.isAnonymous) {
+        // User is signed in with email and password
+        console.log('Navigating to history page');
+        router.push(`/history/${user.uid}`);
+      } else {
+        // User is signed in anonymously
+        // Prompt them to create an account
+        console.log('Showing Auth modal');
+        setShowAuthModal(true);
+      }
+    } else {
+      // User is not signed in
+      // Prompt them to sign in or create an account
+      console.log('Showing Auth modal');
+      setShowAuthModal(true);
+    }
+  };
+
+  const handleUserAuth = (user) => {
+    // Update user state here
+    // Close the Auth modal
+    setShowAuthModal(false);
   };
 
   useHotkeys('shift+l', handleLogout);
@@ -43,6 +79,7 @@ const OptionsMenu = () => {
         </DropdownTrigger>
         <DropdownMenu variant="shadow" aria-label="Options menu">
           <DropdownItem key="logout" shortcut="⇧L" onClick={handleLogout}>Logout</DropdownItem>
+          <DropdownItem key="history" onClick={handleHistory}>Game History</DropdownItem>
         </DropdownMenu>
       </Dropdown>
       <Modal 
@@ -91,6 +128,13 @@ const OptionsMenu = () => {
           )}
         </ModalContent>
       </Modal>
+      {showAuthModal && (
+        <Modal isOpen={showAuthModal} onClose={() => setShowAuthModal(false)}>
+          <ModalContent>
+            <Auth onUserAuth={handleUserAuth} />
+          </ModalContent>
+        </Modal>
+      )}
     </>
   );
 };
