bd03346e9e0b351da90fc1dcd38ba43335ae9915
ryanmio <ryan@mioduski.us>
2023-11-19 18:19:27 -0500

    Update leaderboard.js

--- app/src/pages/leaderboard.js
+++ app/src/pages/leaderboard.js
@@ -1,6 +1,9 @@
 // leaderboard.js
 import { getFirebaseFirestore } from '../firebase';
 import { collection, getDocs } from 'firebase/firestore';
+import { Pagination } from '@nextui-org/react';
+import React, { useState } from 'react';
+import { NextUIProvider } from "@nextui-org/react";
 
 export const getServerSideProps = async () => {
   const db = getFirebaseFirestore();
@@ -20,8 +23,6 @@ export const getServerSideProps = async () => {
   // Sort leaderboard data by score
   leaderboardData.sort((a, b) => b.score - a.score);
 
-  console.log(leaderboardData); // Debug log
-
   return {
     props: {
       leaderboardData: JSON.parse(JSON.stringify(leaderboardData)),
@@ -30,25 +31,55 @@ export const getServerSideProps = async () => {
 };
 
 const LeaderboardPage = ({ leaderboardData }) => {
-  console.log(leaderboardData); // Debug log
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = leaderboardData.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
 
   return (
-    <div>
-      <h1>Leaderboard</h1>
-      <ul>
-        {leaderboardData.map((game) => (
-          <li key={game.id}>
-            {game.leaderboardName}: {game.score}
-            <br />
-            Longest Streak: {game.longestStreak || 0}
-            <br />
-            Date: {game.date ? new Date(game.date).toLocaleDateString() : 'N/A'}
-            <br />
-            Language: {game.language || 'N/A'}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <NextUIProvider>
+      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-8">
+            <h1 className="text-2xl font-bold text-center text-gray-900">Leaderboard</h1>
+            {currentItems.map((game) => (
+              <div key={game.id} className="bg-white p-6 rounded-lg shadow-md mb-4">
+                <div className="flex justify-between items-center mb-4">
+                  <h2 className="text-lg sm:text-xl font-bold text-gray-900">
+                    {game.leaderboardName}: {game.score}
+                  </h2>
+                  <span className="text-sm text-gray-500">
+                    Date: {game.date ? new Date(game.date).toLocaleDateString() : 'N/A'}
+                  </span>
+                </div>
+                <div className="border-t pt-4">
+                  <div className="flex justify-between items-center">
+                    <div className="text-lg text-gray-700">
+                      Longest Streak: {game.longestStreak || 0}
+                    </div>
+                    <div className="text-lg text-gray-700">
+                      Language: {game.language || 'N/A'}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+            <Pagination
+              total={Math.ceil(leaderboardData.length / itemsPerPage)}
+              page={currentPage}
+              onChange={handlePageChange}
+            />
+          </div>
+        </div>
+      </div>
+    </NextUIProvider>
   );
 };
 
