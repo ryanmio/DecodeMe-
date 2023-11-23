b8c316b5c81a939ca4f5d03e0b1a17c487924c82
ryanmio <ryan@mioduski.us>
2023-11-23 15:25:01 -0500

    hearts instead of strikes

--- app/src/components/StrikeIndicator.js
+++ app/src/components/StrikeIndicator.js
@@ -1,13 +1,16 @@
 // StrikeIndicator.js
 import React from 'react';
+import { HeartIcon } from '@heroicons/react/24/solid'; // Ensure you're using the correct version of heroicons
 
 export default function StrikeIndicator({ strikes, limit }) {
   return (
-    <div>
-      {Array.from({ length: limit }, (_, i) => 'X').map((x, index) => (
-        <span key={index} style={{ color: index < strikes ? 'red' : 'lightgray' }}>
-          {x}
-        </span>
+    <div className="flex items-center">
+      {Array.from({ length: limit }, (_, index) => (
+        <HeartIcon
+          key={index}
+          className={`h-6 w-6 ${index < (limit - strikes) ? 'text-red-500' : 'text-gray-300'} mx-1`}
+          style={{ display: index < (limit - strikes) ? 'block' : 'none' }} // Hide the heart if it's a strike
+        />
       ))}
     </div>
   );
