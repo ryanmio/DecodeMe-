8de6e0165ba4fcde383993cbbc02338fae919a4d
ryanmio <ryan@mioduski.us>
2023-11-25 17:51:18 -0500

    Fix: Pass empty question object to NavigationButtons in history page
    In the history page, we were not passing a question object to the NavigationButtons component, which was causing an error. This commit fixes the issue by passing an empty question object. This change ensures that the NavigationButtons component receives the required props and functions as expected.

--- app/src/pages/history/[userId].js
+++ app/src/pages/history/[userId].js
@@ -39,7 +39,7 @@ const HistoryPage = ({ userData, userHistory }) => {
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-8 fixed-width">
-            <NavigationButtons resetGame={resetGame} />
+            <NavigationButtons resetGame={resetGame} question={{}} />
             <div className="results-header mb-4">
               <h1 className="text-2xl font-bold text-center text-gray-900">Game History</h1>
               <p className="text-lg text-center text-gray-700">Leaderboard Name: {userData?.leaderboardName}</p>
