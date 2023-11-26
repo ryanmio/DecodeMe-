ae810cd5ce7f4b19f9bb92e589de3c04195f70d2
ryanmio <ryan@mioduski.us>
2023-11-26 11:15:08 -0500

    chat with script window

--- app/src/components/ChatWithScript.js
+++ app/src/components/ChatWithScript.js
@@ -11,21 +11,25 @@ export default function ChatWithScript({ isOpen, onClose, codeSnippet }) {
   };
 
   return (
-    <div style={{ position: 'fixed', bottom: 0, right: 0, width: '300px', height: '400px', zIndex: 1000, display: isOpen ? 'block' : 'none', backgroundColor: 'white', border: '1px solid #ccc', borderRadius: '10px' }}>
-      <div style={{ padding: '10px', borderBottom: '1px solid #ccc', fontWeight: 'bold' }}>Chat with Script</div>
-      <ScrollShadow style={{ height: 'calc(100% - 60px)', overflowY: 'scroll' }}>
-        {/* Map through chatHistory and display each message */}
-      </ScrollShadow>
-      <form onSubmit={handleChatSubmit} style={{ display: 'flex', padding: '10px', borderTop: '1px solid #ccc' }}>
-        <input 
-          type="text" 
-          value={userMessage} 
-          onChange={e => setUserMessage(e.target.value)} 
-          placeholder="Type your message here..."
-          style={{ flex: 1, marginRight: '10px' }}
-        />
-        <Button type="submit">Send</Button>
-      </form>
+    <div style={{ position: 'fixed', bottom: 0, right: 0, width: '300px', height: isOpen ? '400px' : '30px', zIndex: 1000, backgroundColor: 'white', border: '1px solid #ccc', borderRadius: '10px' }}>
+      <div onClick={onClose} style={{ padding: '10px', borderBottom: '1px solid #ccc', fontWeight: 'bold', cursor: 'pointer' }}>Chat with Script</div>
+      {isOpen && (
+        <>
+          <ScrollShadow style={{ height: 'calc(100% - 60px)', overflowY: 'scroll' }}>
+            {/* Map through chatHistory and display each message */}
+          </ScrollShadow>
+          <form onSubmit={handleChatSubmit} style={{ display: 'flex', padding: '10px', borderTop: '1px solid #ccc' }}>
+            <input 
+              type="text" 
+              value={userMessage} 
+              onChange={e => setUserMessage(e.target.value)} 
+              placeholder="Type your message here..."
+              style={{ flex: 1, marginRight: '10px' }}
+            />
+            <Button type="submit">Send</Button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
