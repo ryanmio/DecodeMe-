87cd0decf9ed38655af77df86ed101aebaef9550
ryanmio <ryan@mioduski.us>
2023-11-26 19:15:24 -0500

    chat text input

--- app/src/components/ChatWithScript.js
+++ app/src/components/ChatWithScript.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
-import { ScrollShadow, Button as NextUIButton } from "@nextui-org/react";
+import React, { useState, useEffect, useRef } from 'react';
+import { ScrollShadow, Textarea, Button as NextUIButton } from "@nextui-org/react";
 
 export default function ChatWithScript({ isOpen, onClose, codeSnippet }) {
   const [chatHistory, setChatHistory] = useState([]);
   const [userMessage, setUserMessage] = useState('');
+  const textAreaRef = useRef(null);
+  const [textAreaHeight, setTextAreaHeight] = useState('auto');
 
   const handleChatSubmit = async (event) => {
     event.preventDefault();
     // Call the new Firebase Cloud Function and update chatHistory
   };
+
+  useEffect(() => {
+    if (!textAreaRef.current) return;
+
+    const scrollHeight = textAreaRef.current.scrollHeight;
+    const lineHeight = parseInt(window.getComputedStyle(textAreaRef.current).lineHeight, 10);
+
+    const lines = userMessage.split('\n').length || 1;
+    const newHeight = `${Math.min(lines * lineHeight, 3 * lineHeight)}px`;
+
+    setTextAreaHeight(newHeight);
+  }, [userMessage]);
 
   return (
     <div className={`chat-window ${isOpen ? 'expanded' : 'collapsed'}`}>
@@ -19,11 +33,12 @@ export default function ChatWithScript({ isOpen, onClose, codeSnippet }) {
             {/* Map through chatHistory and display each message */}
           </ScrollShadow>
           <form onSubmit={handleChatSubmit} className="chat-input">
-            <input
-              type="text"
+            <Textarea
+              ref={textAreaRef}
+              style={{ height: textAreaHeight }}
               value={userMessage}
               onChange={e => setUserMessage(e.target.value)}
-              placeholder="Type your message here..."
+              placeholder="Your message..."
               className="message-input"
             />
             <NextUIButton
