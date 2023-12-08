1a4a9186c38d62e8252b6ab2e68ffb7c532a69cd
ryanmio <ryan@mioduski.us>
2023-12-08 10:18:20 -0500

    Update IncorrectReview.js

--- app/src/components/IncorrectReview.js
+++ app/src/components/IncorrectReview.js
@@ -9,13 +9,13 @@ const IncorrectReview = ({ incorrectAnswers }) => {
 
   return (
     <div className="incorrect-review text-left">
-      <Accordion motionProps={{
+      <Accordion defaultExpandedKeys={["incorrect-results"]} motionProps={{
         variants: {
           enter: { y: 0, opacity: 1, height: "auto", transition: { height: { type: "spring", stiffness: 500, damping: 30, duration: 1 }, opacity: { easings: "ease", duration: 1 } } },
           exit: { y: -10, opacity: 0, height: 0, transition: { height: { easings: "ease", duration: 0.25 }, opacity: { easings: "ease", duration: 0.3 } } },
         },
       }}>
-        <AccordionItem key="incorrect-results" aria-label="Incorrect Results" title="Show Incorrect Responses">
+        <AccordionItem key="incorrect-results" aria-label="Incorrect Results" title="Review Incorrect Responses">
           <div>
             {incorrectAnswers.map((item, index) => {
               const formattedQuestion = formatCodeSnippet(item.question);
