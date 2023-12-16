c7ee575d035013c32da3252722cb9170c9ef23aa
ryanmio <ryan@mioduski.us>
2023-12-16 11:13:24 -0500

    updates to deploy via netlify

--- app/src/utils/firebaseAdmin.js
+++ app/src/utils/firebaseAdmin.js
@@ -3,23 +3,15 @@ import admin from 'firebase-admin';
 
 // Initialize Firebase Admin SDK if it hasn't been initialized yet
 if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
-    databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
-  });
-}
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY, // No need for replace here if newlines are correctly formatted
+      }),
+      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+    });
+  }
 
 const db = admin.firestore();
-
-async function fetchCollection(docRef, collectionName) {
-  const collectionRef = docRef.collection(collectionName);
-  const collectionQuery = collectionRef.orderBy('timestamp', 'desc');
-  const querySnapshot = await collectionQuery.get();
-  return querySnapshot.docs.map(doc => doc.data());
-}
-
 export default db;
