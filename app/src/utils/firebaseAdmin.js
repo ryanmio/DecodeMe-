da78ebc8d2d4ef66e0ddb7f2efdd839d045c13af
ryanmio <ryan@mioduski.us>
2023-12-16 12:18:50 -0500

    Update firebaseAdmin.js

--- app/src/utils/firebaseAdmin.js
+++ app/src/utils/firebaseAdmin.js
@@ -1,17 +1,15 @@
-// utils/firebaseAdmin.js
-import admin from 'firebase-admin';
+// utils/firebaseClient.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Initialize Firebase Admin SDK if it hasn't been initialized yet
-if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY, // No need for replace here if newlines are correctly formatted
-      }),
-      databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
-    });
-  }
+const firebaseConfig = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com`
+};
 
-const db = admin.firestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 export default db;
