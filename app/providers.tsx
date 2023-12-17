e2fef55e8df73ac0ed1be62aca13420211ec5667
ryanmio <ryan@mioduski.us>
2023-12-17 09:01:02 -0500

    small refactor to remove gamestate context

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { NextUIProvider } from '@nextui-org/react';
-import { GameStateProvider } from '../contexts/GameStateContext';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <GameStateProvider>
         {children}
-      </GameStateProvider>
     </NextUIProvider>
   );
 }
