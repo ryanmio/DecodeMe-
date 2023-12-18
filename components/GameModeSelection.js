80ccdcc0af38339e47a0a8bdb9294050dae24161
ryanmio <ryan@mioduski.us>
2023-12-17 19:54:00 -0500

    Update GameModeSelection.js

--- components/GameModeSelection.js
+++ components/GameModeSelection.js
@@ -15,17 +15,20 @@ function GameModeSelection({ onGameModeSelect }) {
     <div className="flex flex-col items-center w-full max-w-md mx-auto mt-4">
       <h1 className="text-2xl font-bold mb-4">Select a Game Mode</h1>
       <ul className="space-y-4">
-        {gameModes.map((mode, index) => (
-          <li key={index} className="w-full">
-            <button 
-              onClick={() => handleGameModeSelect(mode)}
-              className={`w-full text-left py-2 px-4 rounded shadow ${selectedMode === mode ? 'bg-blue-500 text-white' : 'bg-white'} transition-all duration-200 ease-in-out ${mode !== 'Multiple Choice' ? 'cursor-not-allowed opacity-50' : 'hover:shadow-lg hover:scale-105'}`}
-              disabled={mode !== 'Multiple Choice'}
-            >
-              {mode}
-            </button>
-          </li>
-        ))}
+        {gameModes.map((mode) => {
+          const buttonClass = `w-full text-left py-2 px-4 rounded shadow ${selectedMode === mode ? 'bg-blue-500 text-white' : 'bg-white'} transition-all duration-200 ease-in-out ${mode !== 'Multiple Choice' ? 'cursor-not-allowed opacity-50' : 'hover:shadow-lg hover:scale-105'}`;
+          return (
+            <li key={mode} className="w-full">
+              <button 
+                onClick={() => handleGameModeSelect(mode)}
+                className={buttonClass}
+                disabled={mode !== 'Multiple Choice'}
+              >
+                {mode}
+              </button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
