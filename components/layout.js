fadbfd15ef8ddfc48beeb53e245c505d657fa90c
ryanmio <ryan@mioduski.us>
2023-12-18 20:36:01 -0500

    Revert "Update layout.js"
    This reverts commit cf5b6693866e3c0623c094148c6f3d4842cf13a5 and others

--- components/layout.js
+++ components/layout.js
@@ -2,23 +2,25 @@
 import Head from 'next/head'
 import PropTypes from 'prop-types'
 
-const defaultMetadata = {
-  title: 'Default Title',
-  description: 'Default Description',
-  image: '/images/shareimage.jpeg',
-  url: 'Default URL',
-};
+export default function RootLayout({ children, metadata }) {
+  const defaultMetadata = {
+    title: 'Default Title',
+    description: 'Default Description',
+    image: '/images/shareimage.jpeg',
+    url: 'Default URL',
+  };
 
-function RootLayout({ children, metadata }) {
+  const finalMetadata = { ...defaultMetadata, ...metadata };
+
   return (
     <div>
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.image} />
-        <meta property="og:url" content={metadata.url} />
+        <title>{finalMetadata.title}</title>
+        <meta name="description" content={finalMetadata.description} />
+        <meta property="og:title" content={finalMetadata.title} />
+        <meta property="og:description" content={finalMetadata.description} />
+        <meta property="og:image" content={finalMetadata.image} />
+        <meta property="og:url" content={finalMetadata.url} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       {children}
@@ -26,18 +28,21 @@ function RootLayout({ children, metadata }) {
   )
 }
 
-RootLayout.defaultProps = {
-  metadata: defaultMetadata
-};
-
 RootLayout.propTypes = {
   children: PropTypes.node.isRequired,
   metadata: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  }).isRequired,
+    title: PropTypes.string,
+    description: PropTypes.string,
+    image: PropTypes.string,
+    url: PropTypes.string,
+  }),
 }
 
-export default RootLayout;
+RootLayout.defaultProps = {
+  metadata: {
+    title: 'Default Title',
+    description: 'Default Description',
+    image: '/images/shareimage.jpeg',
+    url: 'Default URL',
+  },
+}
