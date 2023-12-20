53a840411626eee75c844d78a49b92e7081e0bba
ryanmio <ryan@mioduski.us>
2023-12-20 08:29:51 -0500

    Revert _app.js to Previous Version Without next/head
    This commit reverts the changes made to _app.js, specifically removing the next/head import and its usage. The previous version of the file did not include next/head, which was introduced to add a favicon link to the application's head. This revert is being done to troubleshoot a 'MODULE_NOT_FOUND' error encountered in production, specifically related to 'next/dist/server/future/route-modules/pages/vendored/contexts

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
-import Head from 'next/head'; // import Head
 import '/style.css';
 import '/tailwind.css';
 
 import { NextUIProvider } from "@nextui-org/react";
 
-// MyApp is the main application component. It wraps the entire application with NextUIProvider.
+// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Component {...pageProps} />
     </NextUIProvider>
   );
