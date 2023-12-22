009d2086b89bb515a9deb24906822634e95656f2
ryanmio <ryan@mioduski.us>
2023-12-22 13:01:38 -0500

    Update results.js

--- pages/results.js
+++ pages/results.js
@@ -5,7 +5,7 @@ import ChallengeSection from '../components/ChallengeSection';
 import GameHistory from '../components/GameHistory';
 import FinalScore from '../components/FinalScore';
 import RootLayout from '../components/layout';
-import SEO from '../components/SEO';
+// import SEO from '../components/SEO';
 
 const ResultsPage = ({ gameData, gameHistory }) => {
   const metadata = {
@@ -17,7 +17,7 @@ const ResultsPage = ({ gameData, gameHistory }) => {
 
   return (
     <RootLayout>
-      <SEO {...metadata} />
+      {/* <SEO {...metadata} /> use the SEO component and pass the metadata as props */}
       <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
