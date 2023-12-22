d0ed49a38953b3433fed2894baed8f9a8c1163b3
ryanmio <ryan@mioduski.us>
2023-12-22 16:23:23 -0500

    Update FinalScore.js

--- components/FinalScore.js
+++ components/FinalScore.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { CircularProgress } from "@nextui-org/react";
 
 const FinalScore = ({ score, questionsAnswered, sharedAt }) => {
+  console.log('sharedAt:', sharedAt);
   const finalScore = Math.round((score / questionsAnswered) * 100);
-  const isFirestoreTimestamp = sharedAt && sharedAt.hasOwnProperty('seconds') && sharedAt.hasOwnProperty('nanoseconds');
-  const date = isFirestoreTimestamp ? sharedAt.toDate().toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' }) : 'Loading...';
+  const isISODateString = sharedAt && typeof sharedAt === 'string';
+  console.log('isISODateString:', isISODateString);
+  const date = isISODateString ? new Date(sharedAt).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' }) : 'Loading...';
+  console.log('date:', date);
   return (
     <div className="flex justify-center items-center mb-4">
       <CircularProgress
