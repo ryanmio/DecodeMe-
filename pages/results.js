51f62de9651f7e6d040bbc0dbb82b687c6fc2667
ryanmio <ryan@mioduski.us>
2023-12-22 14:56:36 -0500

    Update results.js

--- pages/results.js
+++ pages/results.js
@@ -7,35 +7,11 @@ import FinalScore from '../components/FinalScore';
 import RootLayout from '../components/layout';
 
 const ResultsPage = ({ gameData, gameHistory }) => {
-  console.log('Rendering ResultsPage with gameData:', gameData);
-  console.log('Rendering ResultsPage with gameHistory:', gameHistory);
-
-  const metadata = {
-    title: `Game Results for ${gameData?.leaderboardName}`,
-    description: `Check out the game results for ${gameData?.leaderboardName} on DecodeMe!`,
-    image: '/images/shareimage.jpeg',
-    url: `https://deocdeme.app/results/${gameData?.id}`,
-  };
-
   return (
-    <RootLayout metadata={metadata}>
-      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-          <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-8 fixed-width">
-            <div className="results-header mb-4">
-              <h1 className="text-2xl font-bold text-center text-gray-900">Game Results</h1>
-              <p className="text-lg text-center text-gray-700">Leaderboard Name: {gameData?.leaderboardName}</p>
-            </div>
-            <FinalScore score={gameData?.score} questionsAnswered={gameData?.questionsAnswered} sharedAt={gameData?.sharedAt} longestStreak={gameData?.longestStreak} />
-            <GameHistory gameHistory={gameHistory} />
-          </div>
-        </div>
-        <div className="pt-5">
-          <ChallengeSection />
-        </div>
-      </div>
-    </RootLayout>
+    <div>
+      <h1>{gameData?.leaderboardName}</h1>
+      <p>{gameData?.score}</p>
+    </div>
   );
 };
 
