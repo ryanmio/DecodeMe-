0844a915503e0be65485fd96e920d09a6a422c51
ryanmio <ryan@mioduski.us>
2023-12-23 16:04:59 -0500

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -12,3 +12,31 @@ export default function RootLayout({ children }) {
 RootLayout.propTypes = {
   children: PropTypes.node.isRequired,
 }
+
+export const metadata = {
+  title: 'DecodeMe!',
+  description: 'DecodeMe! is a web-based game that helps players understand code snippets in a fun and interactive way.',
+  keywords: ['coding', 'game', 'learning', 'interactive', 'fun'],
+  openGraph: {
+    title: 'DecodeMe!',
+    description: 'DecodeMe! is a web-based game that helps players understand code snippets in a fun and interactive way.',
+    url: 'https://deocdeme.app',
+    siteName: 'DecodeMe!',
+    images: [
+      {
+        url: 'https://deocdeme.app/images/shareimage.jpeg',
+        width: 800,
+        height: 600,
+        alt: 'DecodeMe! Logo',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@DecodeMe',
+    title: 'DecodeMe!',
+    description: 'DecodeMe! is a web-based game that helps players understand code snippets in a fun and interactive way.',
+    image: 'https://deocdeme.app/images/shareimage.jpeg',
+  },
+}
