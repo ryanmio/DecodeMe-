d1f3082a46f18b8ad4daf4290c88dcfcab47f10a
ryanmio <ryan@mioduski.us>
2023-12-24 14:50:55 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <title>DecodeMe!</title>
           <meta name="description" content="DecodeMe! is a web-based game that helps players understand code snippets in a fun and interactive way." />
           <meta name="keywords" content="coding,game,learning,interactive,fun" />
-          <meta property="og:description" content="DecodeMe! is a web-based game that helps players understand code snippets in a fun and interactive way." />
+          {/* <meta property="og:description" content="DecodeMe! is a web-based game that helps players understand code snippets in a fun and interactive way." /> */}
           <meta property="og:url" content="https://decodeme.app" />
           <meta property="og:site_name" content="DecodeMe!" />
           <meta property="og:image" content="https://decodeme.app/images/shareimage.jpeg" />
