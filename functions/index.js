48283430b1ab26327ae7c23d64fcab125d72a6c7
ryanmio <ryan@mioduski.us>
2023-12-28 08:33:55 -0500

    Add Cloud Function to update daily streaks
    This commit adds a new Cloud Function, `updateDailyStreaks`, which is triggered by Cloud Scheduler every day at midnight. The function fetches all users and their game histories, calculates the streak for each user, and updates the user's streak in Firestore. The streak is calculated by sorting the games by date and counting the consecutive days with games.

--- functions/index.js
+++ functions/index.js
@@ -313,3 +313,53 @@ exports.recalculateUserStats = functions.https.onCall(async (data, context) => {
   // Log a message after updating Firestore
   console.log(`Updated stats for user: ${userId} in Firestore`);
 });
+
+
+
+
+
+
+/**
+ * This Cloud Function updates the daily streaks for all users. It is triggered by Cloud Scheduler every day at midnight.
+ * The function fetches all users and their game histories, calculates the streak for each user, and updates the user's streak in Firestore.
+ * The streak is calculated by sorting the games by date and counting the consecutive days with games.
+ */
+exports.updateDailyStreaks = functions.pubsub.schedule('0 0 * * *').onRun(async (context) => {
+
+  // Helper function to check if two dates are on the same day
+  function isSameDay(date1, date2) {
+    return date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate();
+  }
+
+  // Fetch all users
+  const usersSnapshot = await admin.firestore().collection('users').get();
+  const users = usersSnapshot.docs.map(doc => doc.data());
+
+  // Calculate the streak for each user
+  for (const user of users) {
+    const gameHistorySnapshot = await admin.firestore().collection('users').doc(user.id).collection('games').get();
+    const gameHistory = gameHistorySnapshot.docs.map(doc => doc.data());
+
+    // Sort games by date
+    gameHistory.sort((a, b) => b.date - a.date);
+
+    let streak = 0;
+    let currentDate = new Date();
+
+    for (const game of gameHistory) {
+      if (isSameDay(new Date(game.date), currentDate)) {
+        streak++;
+        currentDate.setDate(currentDate.getDate() - 1);
+      } else {
+        break;
+      }
+    }
+
+    // Update the user's streak in Firestore
+    await admin.firestore().collection('users').doc(user.id).update({ streak });
+  }
+
+  console.log('Daily streaks updated for all users');
+});
