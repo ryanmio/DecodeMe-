d9f61771a0b795f28d8416e714f62dbdbaf7b72f
ryanmio <ryan@mioduski.us>
2023-12-30 16:08:36 -0500

    Update [userId].js

--- pages/assistantSettings/[userId].js
+++ pages/assistantSettings/[userId].js
@@ -8,18 +8,24 @@ import { db as dbServer } from '../../firebaseAdmin'; // Server-side db
 
 const AssistantSettingsPage = ({ userData }) => {
   const router = useRouter();
-  const [customInstructions, setCustomInstructions] = useState(userData?.customInstructions || '');
+  const [customInstructions, setCustomInstructions] = useState({
+    codeGen: userData?.customInstructions?.codeGen || '',
+    chatbot: userData?.customInstructions?.chatbot || '',
+    endGame: userData?.customInstructions?.endGame || '',
+  });
 
-  const handleInputChange = (event) => {
-    setCustomInstructions(event.target.value);
+  const handleInputChange = (aspect, event) => {
+    const value = event.target.value;
+    setCustomInstructions(prevInstructions => ({
+      ...prevInstructions,
+      [aspect]: value,
+    }));
   };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    // Update the customInstructions field in Firestore
     const userDocRef = dbClient.collection('users').doc(userData.id);
     await userDocRef.update({ customInstructions });
-    // Redirect to the user stats page
     router.push(`/user/${userData.id}`);
   };
 
@@ -29,15 +35,42 @@ const AssistantSettingsPage = ({ userData }) => {
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
             <h1 className="text-2xl font-bold text-gray-900">Assistant Settings</h1>
-            <form onSubmit={handleFormSubmit}>
-              <Input
-                value={customInstructions}
-                onChange={handleInputChange}
-                placeholder="Enter your custom instructions"
-              />
-              <Button color="primary" auto style={{ backgroundColor: '#007BFF', color: 'white' }} type="submit">
-                Save
-              </Button>
+            <form onSubmit={handleFormSubmit} style={{ maxWidth: '500px', margin: '0 auto' }}>
+              <div className="input-group" style={{ marginBottom: '20px' }}>
+                <label htmlFor="codeGen">Code Generation Instructions:</label>
+                <Input
+                  id="codeGen"
+                  value={customInstructions.codeGen}
+                  onChange={(e) => handleInputChange('codeGen', e)}
+                  placeholder="Enter your code generation instructions"
+                  fullWidth
+                />
+              </div>
+              <div className="input-group" style={{ marginBottom: '20px' }}>
+                <label htmlFor="chatbot">Chatbot Instructions:</label>
+                <Input
+                  id="chatbot"
+                  value={customInstructions.chatbot}
+                  onChange={(e) => handleInputChange('chatbot', e)}
+                  placeholder="Enter your chatbot instructions"
+                  fullWidth
+                />
+              </div>
+              <div className="input-group" style={{ marginBottom: '20px' }}>
+                <label htmlFor="endGame">End Game Message Instructions:</label>
+                <Input
+                  id="endGame"
+                  value={customInstructions.endGame}
+                  onChange={(e) => handleInputChange('endGame', e)}
+                  placeholder="Enter your end game message instructions"
+                  fullWidth
+                />
+              </div>
+              <div style={{ textAlign: 'right', marginTop: '20px' }}>
+                <Button color="primary" auto type="submit">
+                  Save
+                </Button>
+              </div>
             </form>
           </div>
         </div>
