a819915d10fe8eb0b02396705a2034d46172e465
ryanmio <ryan@mioduski.us>
2023-12-31 18:39:08 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -467,6 +467,7 @@ exports.checkUsageData = functions.firestore.document('users/{userId}').onUpdate
 /**
  * This Cloud Function resets the usage data for non-anonymous users. It is triggered by Cloud Scheduler every day at midnight.
  * The function fetches all users and sets their gptCalls and gptTokens to 0 if they are not anonymous.
+ * Before resetting, it increments lifetimeGptCalls and lifetimeGptTokens fields by the current values of gptCalls and gptTokens.
  */
 exports.resetUsageData = functions.pubsub.schedule('0 0 * * *').onRun(async (context) => {
   // Fetch all users
@@ -485,7 +486,11 @@ exports.resetUsageData = functions.pubsub.schedule('0 0 * * *').onRun(async (con
       console.log(`Resetting usage data for user ${user.id}`);
 
       const userRef = admin.firestore().collection('users').doc(user.id);
+      
+      // Increment lifetimeGptCalls and lifetimeGptTokens by the current values of gptCalls and gptTokens
       batch.update(userRef, {
+        lifetimeGptCalls: admin.firestore.FieldValue.increment(user.gptCalls || 0),
+        lifetimeGptTokens: admin.firestore.FieldValue.increment(user.gptTokens || 0),
         gptCalls: 0,
         gptTokens: 0,
         capExceeded: false
