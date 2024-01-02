2aa9222ca76e80ca6a84fffd49490fc779febf3f
ryanmio <ryan@mioduski.us>
2024-01-02 18:19:06 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -321,7 +321,7 @@ export default function Home() {
             </div>
             {gameMode && <div className="flex justify-center"><StrikeIndicator strikes={strikes} limit={strikeLimit} /></div>}
           </h1>
-          <div className="auth-container">
+          <div className="auth-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             {isLoading ? <Spinner label="Initializing..." color="warning" /> : (!user ? <Auth onUserAuth={handleUserUpdate} onLeaderboardNameSet={setLeaderboardName} /> : 
             !gameMode ? (
               <>
