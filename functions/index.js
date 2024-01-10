91af61db5c625db72243409a399f55a35c323b6e
ryanmio <ryan@mioduski.us>
2024-01-10 18:07:43 -0500

    Refactor import paths and add instructions for adding new functions
    This commit updates the import paths in index.js to correctly reference the individual function files in the same directory. Previously, the paths were incorrectly prefixed with ./functions/, which caused the Firebase Emulator Suite to fail to load the functions.
    
    Additionally, this commit adds detailed instructions in index.js on how to add new functions. These instructions guide the developer through the process of creating a new function file, writing and exporting the function, importing and re-exporting the function in index.js, testing the function locally with the Firebase Emulator Suite, and finally deploying the function to Firebase.

--- functions/index.js
+++ functions/index.js
@@ -6,12 +6,33 @@ admin.initializeApp();
 const db = admin.firestore();
 exports.db = db;
 
-exports.getCodeSnippet = require('./functions/getCodeSnippet').getCodeSnippet;
-exports.updateLeaderboard = require('./functions/updateLeaderboard').updateLeaderboard;
-exports.chatWithScript = require('./functions/chatWithScript').chatWithScript;
-exports.fetchPostGameMessage = require('./functions/fetchPostGameMessage').fetchPostGameMessage;
-exports.recalculateUserStats = require('./functions/recalculateUserStats').recalculateUserStats;
-exports.updateDailyStreaks = require('./functions/updateDailyStreaks').updateDailyStreaks;
-exports.checkUsageData = require('./functions/checkUsageData').checkUsageData;
-exports.resetUsageData = require('./functions/resetUsageData').resetUsageData;
-exports.initializeUserCaps = require('./functions/initializeUserCaps').initializeUserCaps;
+exports.getCodeSnippet = require('./getCodeSnippet').getCodeSnippet;
+exports.updateLeaderboard = require('./updateLeaderboard').updateLeaderboard;
+exports.chatWithScript = require('./chatWithScript').chatWithScript;
+exports.fetchPostGameMessage = require('./fetchPostGameMessage').fetchPostGameMessage;
+exports.recalculateUserStats = require('./recalculateUserStats').recalculateUserStats;
+exports.updateDailyStreaks = require('./updateDailyStreaks').updateDailyStreaks;
+exports.checkUsageData = require('./checkUsageData').checkUsageData;
+exports.resetUsageData = require('./resetUsageData').resetUsageData;
+exports.initializeUserCaps = require('./initializeUserCaps').initializeUserCaps;
+
+/*
+ * Instructions for adding new functions:
+ * 
+ * 1. In the functions directory, create a new JavaScript file for your function. The filename should be descriptive of the function's purpose (e.g., `sendEmail.js`).
+ * 
+ * 2. In the new file, write your Cloud Function following the Firebase Functions API. Make sure to export your function at the end of the file. For example:
+ * 
+ *    const functions = require('firebase-functions');
+ *    exports.myNewFunction = functions.https.onRequest((request, response) => {
+ *      // Your function logic here
+ *    });
+ * 
+ * 3. In your `index.js` file, import and re-export the new function. Add a line like this:
+ * 
+ *    exports.myNewFunction = require('./myNewFunction').myNewFunction;
+ * 
+ * 4. Use the Firebase Emulator Suite to test your function locally before deploying it.
+ * 
+ * 5. Once you're confident that your function works as expected, deploy it to Firebase.
+ */
