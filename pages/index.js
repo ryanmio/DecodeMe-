1c014622fee19472b459f69e4f6d99a9d5e53758
ryanmio <ryan@mioduski.us>
2024-01-14 18:58:11 -0500

    footer

--- pages/index.js
+++ pages/index.js
@@ -378,6 +378,9 @@ export default function Home() {
           )}
         </div>
       </div>
+      <footer className="text-center p-4 absolute bottom-0 w-full text-center text-gray-500 text-sm">
+        Made by <a href="https://github.com/ryanmio" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-700 hover:underline">Ryan</a>
+      </footer>
     </div>
   );
 }
