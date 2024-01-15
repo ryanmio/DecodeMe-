9f234c55fb528038645e06fcf14667f88711f5de
ryanmio <ryan@mioduski.us>
2024-01-15 09:46:59 -0500

    Fix mobile overflow on LeaderboardPage
    Addressed an issue on the LeaderboardPage where content exceeded the viewport width on mobile devices, causing an overflow. Changes were made to ensure responsiveness and proper scaling of elements across all screen sizes.
    
    - Removed the fixed-width class from the main container div and replaced it with w-full sm:w-auto, allowing the container to adjust its width dynamically based on the viewport size.
    - Applied min-h-screen w-screen bg-gray-100 p-0 m-0 flex flex-col justify-center to the main div to eliminate default margins and padding, ensuring the background design extends fully to the viewport edges on mobile.
    
    These updates prevent horizontal overflow on mobile devices and enhance the user experience by ensuring the layout adapts correctly to various screen sizes.

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -132,10 +132,10 @@ const LeaderboardPage = ({ leaderboardData, error }) => {
         <meta property="og:image" content="https://decodeme.app/images/shareimage.png" />
         <meta property="og:url" content="https://decodeme.app/leaderboard" />
       </Head>
-      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center">
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-8 fixed-width">
+          <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-8 w-full sm:w-auto">
             <NavigationButtons resetGame={resetGame} question={{}} />
             <h1 className="text-2xl font-bold text-center text-gray-900 mb-4">Leaderboard</h1>
             <div className="flex justify-center mb-4">
