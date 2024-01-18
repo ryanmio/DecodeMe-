3d144640a03206448ed84a5823fb3abf7f4cf779
ryanmio <ryan@mioduski.us>
2024-01-18 08:53:00 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import '/style.css';
 import '/tailwind.css';
-import Script from 'next/script'; // Import the Script component
+import Script from 'next/script';
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from '../contexts/AuthContext';
 
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID; // Your GA Measurement ID
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
 export default function MyApp({ Component, pageProps }) {
   return (
