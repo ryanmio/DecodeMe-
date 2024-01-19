29905d852fc7aca5aa5cbd51ef286814929e59e0
ryanmio <ryan@mioduski.us>
2024-01-19 10:45:06 -0500

    Integrate next-google-analytics in _app.js
    Updated the _app.js file to remove the previous analytics script and replaced it with the next-google-analytics component. This change ensures that our analytics tracking is more robust and leverages the latest GA4 features for better insights and performance.

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,18 @@
 // pages/_app.js
 import '/style.css';
 import '/tailwind.css';
-import Script from 'next/script';
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from '../contexts/AuthContext';
 import ErrorBoundary from '../components/ErrorBoundary';
-
-const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
       <NextUIProvider>
         {/* Google Analytics */}
-        <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
+        <GoogleAnalytics trackPageViews />
         {/* End Google Analytics */}
         <Toaster position="top-right" />
         <ErrorBoundary>
