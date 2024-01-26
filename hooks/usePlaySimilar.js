23136a3ee6ee929c757e4b0b4bf752d955755f45
ryanmio <ryan@mioduski.us>
2024-01-26 12:53:44 -0500

    Update usePlaySimilar.js

--- hooks/usePlaySimilar.js
+++ hooks/usePlaySimilar.js
@@ -5,8 +5,15 @@ import { useRouter } from 'next/router';
 const usePlaySimilar = () => {
   const router = useRouter();
 
-  const handlePlaySimilar = (selectedScript) => {
+  const handlePlaySimilar = (selectedScript, resetGame) => {
     console.log('usePlaySimilar - handlePlaySimilar called with:', selectedScript);
+
+    // Reset the game state here if resetGame is a function
+    if (typeof resetGame === 'function') {
+      resetGame(false); // Reset the game without clearing local storage
+    }
+
+    // Set up "Play Similar" mode in local storage
     localStorage.setItem('selectedScriptForSimilarGame', JSON.stringify(selectedScript));
     localStorage.setItem('playSimilar', 'true');
     
