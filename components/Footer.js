0ed710da070f6b3cf969fb3279c0041afefdce93
ryanmio <ryan@mioduski.us>
2024-01-27 19:34:53 -0500

    footer refactored to new component

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+// components/Footer.js
+export default function Footer() {
+    return (
+      <footer className="text-center p-4 mt-8 absolute bottom-0 w-full text-center text-gray-400 text-sm">
+        Made by <a href="https://github.com/ryanmio" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-700 hover:underline">Ryan</a>
+      </footer>
+    );
+  }
