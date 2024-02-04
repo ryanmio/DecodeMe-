a9e8d984ee5d9be1e976384d1eec305c5c5c2d45
ryanmio <ryan@mioduski.us>
2024-02-04 10:29:52 -0500

    sytnax highlighting toggle button

--- components/CodeSnippetDisplay.js
+++ components/CodeSnippetDisplay.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { useSpring, animated } from '@react-spring/web';
 import { Ping } from '@uiball/loaders';
 import Prism from 'prismjs';
+import { Checkbox, CheckboxGroup } from '@nextui-org/react'; // Import NextUI components
 
 // A map to keep track of which languages have been imported
 const loadedLanguages = {};
@@ -13,8 +14,12 @@ export default function CodeSnippetDisplay({ codeSnippet, loading, language = 'p
   const fadeLoading = useSpring({ from: { opacity: 0 }, to: { opacity: 1 }, delay: 200, config: { tension: 200, friction: 20 } });
   const placeholder = " ".repeat(70) + "\n".repeat(4);
 
+  // New state hooks for toggles
+  const [isSyntaxHighlightingEnabled, setIsSyntaxHighlightingEnabled] = useState(true);
+  const [isLineWrappingEnabled, setIsLineWrappingEnabled] = useState(true);
+
   // Define descriptions
-  const descriptions = ["generating...", "validating...", "thinking...", "almost there..."];
+  const descriptions = ["generating...", "validating...", "parsing...", "almost there..."];
   const [currentDescription, setCurrentDescription] = useState(descriptions[0]);
 
   useEffect(() => {
@@ -33,53 +38,74 @@ export default function CodeSnippetDisplay({ codeSnippet, loading, language = 'p
   }, [loading, descriptions]);
 
   useEffect(() => {
-    // Process the code snippet to ensure it's properly formatted for display
     const processedCodeSnippet = codeSnippet?.replace(/```python\n|```python|```/g, '').trim() || placeholder;
     setFormattedCodeSnippet(processedCodeSnippet);
   }, [codeSnippet]);
 
   useEffect(() => {
-    // Function to dynamically load a language only once
-    const loadPrismLanguage = async (language) => {
-      if (!loadedLanguages[language]) {
-        try {
-          await import(`prismjs/components/prism-${language}`);
-          loadedLanguages[language] = true;
-          // Ensure the code is highlighted after the language is loaded
-          if (codeRef.current) {
-            Prism.highlightElement(codeRef.current);
-          }
-        } catch (e) {
-          console.error(`Error loading Prism language: ${language}`, e);
-        }
+    const applyHighlighting = () => {
+      if (isSyntaxHighlightingEnabled && language && Prism.languages[language] && codeRef.current) {
+        Prism.highlightElement(codeRef.current);
       }
     };
 
-    if (language && Prism.languages[language]) {
-      // If the language is already loaded by Prism, no need to import
-      Prism.highlightElement(codeRef.current);
-    } else {
-      // Load the Prism language dynamically
+    const loadPrismLanguage = async (language) => {
+      if (isSyntaxHighlightingEnabled && !loadedLanguages[language]) {
+        try {
+          await import(`prismjs/components/prism-${language}`);
+          loadedLanguages[language] = true;
+          applyHighlighting(); // Ensure highlighting is applied after loading
+        } catch (e) {
+          console.error(`Error loading Prism language: ${language}`, e);
+        }
+      } else {
+        applyHighlighting(); // Apply highlighting if already loaded
+      }
+    };
+
+    if (isSyntaxHighlightingEnabled) {
       loadPrismLanguage(language);
+    } else {
+      // Clear highlighting or reset the code snippet to its unhighlighted state
+      // This might involve setting the innerHTML of the codeRef element to its plain text
+      if (codeRef.current) {
+        codeRef.current.innerHTML = formattedCodeSnippet;
+      }
     }
-  }, [formattedCodeSnippet, language]);
+  }, [formattedCodeSnippet, language, isSyntaxHighlightingEnabled]);
+
+  // Adjust styles based on isLineWrappingEnabled
+  const codeStyle = {
+    whiteSpace: isLineWrappingEnabled ? 'pre-wrap' : 'pre',
+    overflowWrap: isLineWrappingEnabled ? 'break-word' : 'normal',
+  };
 
   return (
     <div>
       <h2 className="text-xl font-medium mb-3 text-left text-gray-900">Code Snippet</h2>
+      {/* CheckboxGroup for toggling options */}
+      <CheckboxGroup
+        value={[isSyntaxHighlightingEnabled ? "syntaxHighlighting" : "", isLineWrappingEnabled ? "lineWrapping" : ""].filter(Boolean)}
+        onChange={(values) => {
+          console.log("Toggled Values:", values); // Add this line
+          setIsSyntaxHighlightingEnabled(values.includes("syntaxHighlighting"));
+          setIsLineWrappingEnabled(values.includes("lineWrapping"));
+        }}
+      >
+        <Checkbox value="syntaxHighlighting">Syntax Highlighting</Checkbox>
+        <Checkbox value="lineWrapping">Line Wrapping</Checkbox>
+      </CheckboxGroup>
       <div className="max-w-[600px] mx-auto relative">
-      <pre style={{
-          minWidth: '300px',
-          minHeight: '250px',
-          whiteSpace: 'pre-wrap',
-          wordBreak: 'normal',
-          overflowWrap: 'break-word',
-          background: '#f6f8fa',
-          padding: '1em',
-          borderRadius: '5px',
-          border: '1px solid #ddd',
-          overflowX: 'auto'
-        }}>
+        <pre style={{
+            minWidth: '300px',
+            minHeight: '25px',
+            background: '#f6f8fa',
+            padding: '1em',
+            borderRadius: '5px',
+            border: '1px solid #ddd',
+            overflowX: 'auto',
+            ...codeStyle
+          }}>
           <code ref={codeRef} className={`language-${language}`} style={{
             whiteSpace: 'pre-wrap',
             overflowWrap: 'break-word'
